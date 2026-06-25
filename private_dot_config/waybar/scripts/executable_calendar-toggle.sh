b93a6bb99c20ac99f796474196e8db05e64681d9
Jorgen V <mail@jvik.no>
2026-06-26 00:38:57 +0200

    Fixed calendar

--- private_dot_config/waybar/scripts/executable_calendar-toggle.sh
+++ private_dot_config/waybar/scripts/executable_calendar-toggle.sh
@@ -5,7 +5,7 @@ APP_ID="waybar-calendar"
 if swaymsg -t get_tree | grep -q "\"app_id\": \"$APP_ID\""; then
     swaymsg "[app_id=$APP_ID] kill"
 else
-    wezterm \
+    flatpak run org.wezfurlong.wezterm \
         --config 'initial_rows = 12' --config 'initial_cols = 28' \
         start --class "$APP_ID" \
         -- bash "$HOME/.config/waybar/scripts/calendar-view.sh"
